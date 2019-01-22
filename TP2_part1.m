0fc94b11f03c96095df4aa16f72136ddea878ea6
colapala <44379582+colapala@users.noreply.github.com>
2019-01-22 18:47:15 +0100

    revue du 22.01

--- TP2_part1.m
+++ TP2_part1.m
@@ -9,26 +9,40 @@ size(map)
 %imNG=0.3*im(:,:,1)+0.59*im(:,:,2)+0.11*im(:,:,3);
 %size(imNG)
 
-%mapNG=([0:255]'/255)*[1 1 1];
+mapNG=([0:255]'/255)*[1 1 1];
 %imNG=imNG(1:512,100:611);
-%colormap(mapNG)
+colormap(mapNG)
 
 IMNG=fft2(im);
 IMNG2=fftshift(IMNG);
 
-figure(3)
-surf(1:256,1:256,log(abs(IMNG2)))
 
+%size(IMNG2)
+%surf(1:256,1:256,log(abs(IMNG2)))
+image_log = log(abs(IMNG2));
 
+maxImage = max(max(image_log));
+minImage = min(min(image_log));
+
+for i = 1:256
+    for j = 1:256
+        image_log(i,j) = 255*(image_log(i,j)-minImage)/(maxImage-minImage);
+    end
+end
+figure(1)
+image(image_log)
+colormap(mapNG)
+
+a=0.005;
 %filtrage pase-bas gaussien
 for i=1:256 
    for j=1:256
-      IMNGF(i,j)= IMNG2(i,j)*exp(-0.005*((i-129)*(i-129)+(j-129)*(j-129)));
+      IMNGF(i,j)= IMNG2(i,j)*exp(-a*((i-129)*(i-129)+(j-129)*(j-129)));
    end
 end
 
 %flitrage passe bas tout ou rien
-k=30;
+k=15;
 for i=129-k:129+k
    for j=129-k:129+k
       IMNGFTR(i,j) = IMNG2(i,j); 
@@ -42,10 +56,10 @@ IMNG3=fftshift(IMNGFTR);
 IMNGInv2 = ifft2(IMNG3);
 
 size(IMNGInv)
-figure(1)
+figure(2)
 image(abs(IMNGInv))
-
 colormap(mapNG)
 
-figure(2)
+figure(3)
 image(abs(IMNGInv2))
+colormap(mapNG)
--- TP2_part2.m
+++ TP2_part2.m
@@ -5,48 +5,88 @@ FiltreC=zeros(1024);
 size(im)
 size(map)
 
-MatRond=zeros(1024);
-MatCheck=zeros(1024);
+MatRond_Vert=zeros(1024);
+MatCheck_Vert=zeros(1024);
+MatRond_Hor=zeros(1024);
+MatCheck_Hor=zeros(1024);
 
 [imR,map1]=imread('puissance6_rond.png');
 [imC,map2]=imread('puissance6_check.png');
 
-for i=509:516
-   for j=509:516
-      MatRond(i,j)=imR(i-508,j-508);
-      MatCheck(i,j)=imC(i-508,j-508);
-   end
+%vertical
+for k=0:8:40
+    for i=489+k:496+k
+     for j=509:516
+      MatRond_Vert(i,j)=imR(i-488-k,j-508);
+      MatCheck_Vert(i,j)=imC(i-488-k,j-508);
+     end
+    end
+end
+
+%horizontal
+for k=0:8:40
+     for i=509:516
+         for j=489+k:496+k
+            MatRond_Hor(i,j)=imR(i-508,j-488-k);
+             MatCheck_Hor(i,j)=imC(i-508,j-488-k);
+         end
+    end
 end
 
 flipud(im);
 fliplr(im);
 
+%Transformée de fourier
 imDep=fft2(im);
-imRond=fft2(MatRond);
-imCheck=fft2(MatCheck);
+imRond_Vert=fft2(MatRond_Vert);
+imCheck_Vert=fft2(MatCheck_Vert);
+imCheck_Hor=fft2(MatCheck_Hor);
+imRond_Hor=fft2(MatRond_Hor);
 
-CorrRond=imRond.*imDep;
-CorrCheck=imCheck.*imDep;
+%produit de corrélation
+CorrRond_Vert=imRond_Vert.*imDep;
+CorrCheck_Vert=imCheck_Vert.*imDep;
+CorrRond_Hor=imRond_Hor.*imDep;
+CorrCheck_Hor=imCheck_Hor.*imDep;
 
 %transformée inverse
-Rond=ifft2(CorrRond);
-Check=ifft2(CorrCheck);
+Rond_Vert=ifft2(CorrRond_Vert);
+Check_Vert=ifft2(CorrCheck_Vert);
+Rond_Hor=ifft2(CorrRond_Hor);
+Check_Hor=ifft2(CorrCheck_Hor);
 
-k=0;
-l=0;
+
+compt_Rond=0;
+compt_Check=0;
+%max
+max_Check_H=max(max(Check_Hor));
+max_Check_V=max(max(Check_Vert));
+max_Rond_V=max(max(Rond_Vert));
+max_Rond_H=max(max(Rond_Hor));
+erreur=0.94;
+
 for i=1:1024
     for j=1:1024
-        if(Rond(i,j)>1e6)
-           FiltreR(i,j)=Rond(i,j); 
-           k=k+1;
+        if(Rond_Vert(i,j)>erreur*max_Rond_V)
+           compt_Rond=compt_Rond+1;
         end
-        if(Check(i,j)>8e5)
-           FiltreC(i,j)=Check(i,j); 
-           l=l+1;
+       if(Rond_Hor(i,j)>erreur*max_Rond_H) 
+           compt_Rond=compt_Rond+1;
+       end
+        if(Check_Vert(i,j)>erreur*max_Check_V) 
+           compt_Check=compt_Check+1;
+        end
+        if(Check_Hor(i,j)>erreur*max_Check_H) 
+           compt_Check=compt_Check+1;
         end
     end
 end
 
-surf(abs(FiltreR))
-k
-l
+compt_Rond
+compt_Check
+
+if (compt_Rond>compt_Check)
+    disp("Les Ronds ont gagné")
+else
+    disp("Les Checks ont gagné")
+end
